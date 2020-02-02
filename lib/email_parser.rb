ec08b4cadfebd4b83619ea011f3e367a94ea9cd0
Abel Kalum <chumbabel@gmail.com>
2020-02-02 13:18:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,4 +6,13 @@ class EmailAddressParser
   
   attr_accessor :email_address
   
+  def initialize
+    @email_address = email_address
+  end
+  
+  def parse
+    email_address.uniq
+  end
+    
+  
 end
